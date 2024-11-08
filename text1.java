5feb7f16c4e3433c74d53b373762545eaf879834
SangmeshMehter <ssmcmethre3080@gmail.com>
2024-11-08 14:18:24 +0530

    file adding

--- text1.java
+++ text1.java
@@ -2,9 +2,8 @@ public class Text1
 {
 	public void m1(){
 		System.out.println("M1 method");
+		System.out.println("Changing the changes");
 	}
 
-	public void m2(){
-		System.out.println("m2 method");
-	}
+	
 }
